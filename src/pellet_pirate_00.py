0cf68731f76f0039bcf2dc3ec10347bf512cc043
akekesi <kekesi.att@gmail.com>
2024-12-31 09:36:35 +0100

    add get_surrounding_coords()

--- src/pellet_pirate_00.py
+++ src/pellet_pirate_00.py
@@ -5,33 +5,61 @@ import networkx as nx
 TEAM_NAME = 'Pellet Pirates'
 BOT_1 = ["a", "x"]
 BOT_2 = ["b", "y"]
+DISTANCE = 3
 
 
 def get_shortest_path(graph: nx.Graph, source: tuple[int, int], targets: list[tuple[int, int]]) -> list[tuple[int, int]]:
     length = float("inf")
     shortest_path = []
     for target in targets:
-        shortest_path_ = nx.shortest_path(G=graph, source=source, target=target)
-        length_ = len(shortest_path_)
+        try:
+            # node reachable
+            shortest_path_ = nx.shortest_path(G=graph, source=source, target=target)
+            length_ = len(shortest_path_)
+        except:
+            # node not reachable
+            continue
         if  length_ < length:
             length = length_
             shortest_path = shortest_path_
     return shortest_path
 
 
+def get_surrounding_coords(point: tuple[int, int], distance: int) -> list[tuple[int, int]]:
+    x, y = point
+    surrounding_coords = [
+        (x + dx, y + dy)
+        for dx in range(-distance, distance + 1)
+        for dy in range(-distance, distance + 1)
+        if abs(dx) + abs(dy) <= distance
+    ]
+    return surrounding_coords
+
+
 def move(bot, state):
     graph = bot.graph.copy()
-    enemies = [
-        bot.enemy[0],
-        bot.enemy[1]
-    ]
+    enemies = bot.enemy
 
     # attacker
     if bot.char in BOT_1:
+        # optional: after death maybe try to catch attacker
         # shortest path to the nearest pellet
         foods = copy.deepcopy(bot.enemy[0].food)
+        enemies_noisy = [enemy for enemy in enemies if not enemy.is_noisy]
+        nodes_delete = []
+        for enemy in enemies_noisy:
+            nodes_delete += get_surrounding_coords(point=enemy.position, distance=DISTANCE)
+        for node_delete in nodes_delete:
+            if node_delete in enemies[0].homezone and node_delete != bot.position:
+                if node_delete in graph:
+                    graph.remove_node(node_delete)
+                if node_delete in foods:
+                    foods.remove(node_delete)
         shortes_path = get_shortest_path(graph=graph, source=bot.position, targets=foods)
-        next_position = shortes_path[1]
+        if shortes_path:
+            next_position = shortes_path[1]
+        else:
+            next_position = bot.random.choice(bot.legal_positions)
 
     # defender
     if bot.char in BOT_2:
